d65b5ee606083b94ba3dc3a9a62a1b3b4221f055
Shashank8602 <shashu.s08@gmail.com>
2025-03-16 15:15:47 +0530

    Entire folder commit

---
+++ src/main/java/com/BlogCapstoneProjectApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogCapstoneProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogCapstoneProjectApplication.class, args);
+	}
+
+}

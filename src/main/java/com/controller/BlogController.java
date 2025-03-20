8afa347f9b935c83c7e4cc457ec50dc89268f010
Shashank8602 <shashu.s08@gmail.com>
2025-03-20 14:18:41 +0530

    Controller modified

--- src/main/java/com/controller/BlogController.java
+++ src/main/java/com/controller/BlogController.java
@@ -16,10 +16,13 @@ import com.dto.CommentDTO;
 import com.service.BlogService;
 import com.service.CommentService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/blogs")
+@Tag(name = "Blog Controller", description = "APIs for managing blogs and comments")
 public class BlogController {
 
 	private final BlogService blogService;
@@ -32,30 +35,35 @@ public class BlogController {
 	}
 
 	@PostMapping
+	@Operation(summary = "Create New Blog", description = "Create a new blog")
 	public ResponseEntity<BlogDTO> createNewBlog(@Valid @RequestBody BlogDTO blogDto) {
 		BlogDTO blog = blogService.addBlog(blogDto);
 		return new ResponseEntity<BlogDTO>(blog, HttpStatus.CREATED);
 	}
 
 	@GetMapping("/{id}")
+	@Operation(summary = "Get Blog by ID", description = "Fetch a specific blog using its ID")
 	public ResponseEntity<BlogDTO> getBlogById(@PathVariable Long id) {
 		BlogDTO blog = blogService.getBlogById(id);
 		return new ResponseEntity<BlogDTO>(blog, HttpStatus.FOUND);
 	}
 
 	@PutMapping("/{id}")
+	@Operation(summary = "Update Blog", description = "Update an existing blog by its ID")
 	public ResponseEntity<BlogDTO> updateBlog(@PathVariable Long id,@Valid @RequestBody BlogDTO blogDto) {
 		BlogDTO blog = blogService.modifyBlog(id, blogDto);
 		return new ResponseEntity<BlogDTO>(blog, HttpStatus.OK);
 	}
 
 	@DeleteMapping("/{id}")
+	@Operation(summary = "Delete Blog", description = "Delete a blog using its ID")
 	public ResponseEntity<Boolean> deleteBlog(@PathVariable Long id) {
 		boolean deleteStatus = blogService.deleteBlog(id);
 		return new ResponseEntity<Boolean>(deleteStatus, HttpStatus.OK);
 	}
 
 	@PostMapping("/comment")
+	@Operation(summary = "Add Comment to Blog", description = "Add a comment to a blog using blog's ID")
 	public ResponseEntity<CommentDTO> createNewComment(@Valid @RequestBody CommentDTO commentDto) {
 		CommentDTO comment = commentService.addComment(commentDto);
 		return new ResponseEntity<CommentDTO>(comment, HttpStatus.CREATED);
